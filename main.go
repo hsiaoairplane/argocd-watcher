75234d6e03777083cc1066fc5e0b3c2e7f30125a
JenTing Hsiao <hsiaoairplane@gmail.com>
2024-11-21 20:15:24 +0800

    update

--- main.go
+++ main.go
@@ -30,50 +30,52 @@ func main() {
 	config := ctrl.GetConfigOrDie()
 	dynamic := dynamic.NewForConfigOrDie(config)
 	namespace := "argocd"
-	items, err := GetResourcesDynamically(dynamic, ctx, "argoproj.io", "v1alpha1", "applications", namespace)
+
+	appList, err := dynamic.Resource(schema.GroupVersionResource{
+		Group:    "argoproj.io",
+		Version:  "v1alpha1",
+		Resource: "applications",
+	}).Namespace(namespace).List(ctx, metav1.ListOptions{})
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		for _, item := range items {
-			// Convert object to raw JSON
-			var rawJson interface{}
-			err = runtime.DefaultUnstructuredConverter.FromUnstructured(item.Object, &rawJson)
-			if err != nil {
-				fmt.Printf("Error converting object to raw JSON: %v\n", err)
-				return
-			}
+	}
 
-			fmt.Printf("Kind: %s, Name: %s/%s\n", item.GetKind(), item.GetName(), item.GetNamespace())
-
-			// print spec
-			spec, found, err := unstructured.NestedMap(item.Object, "spec")
-			if err != nil {
-				fmt.Printf("Error getting spec: %v\n", err)
-				return
-			}
-			if found {
-				fmt.Printf("Spec: %+v\n", spec)
-			}
-
-			// print spec.namespace
-			specNamespace, found, err := unstructured.NestedString(item.Object, "spec.namespace")
-			if err != nil {
-				fmt.Printf("Error getting spec.namespace: %v\n", err)
-				return
-			}
-			if found {
-				fmt.Printf("Spec.Namespace: %s\n", specNamespace)
-			}
-
-			// print spec.server
-			specServer, found, err := unstructured.NestedString(item.Object, "spec.server")
-			if err != nil {
-				fmt.Printf("Error getting spec.server: %v\n", err)
-				return
-			}
-			if found {
-				fmt.Printf("Spec.Server: %s\n", specServer)
-			}
+	for _, item := range appList.Items {
+		// Convert object to raw JSON
+		var rawJson interface{}
+		err = runtime.DefaultUnstructuredConverter.FromUnstructured(item.Object, &rawJson)
+		if err != nil {
+			fmt.Printf("Error converting object to raw JSON: %v\n", err)
+			return
 		}
+
+		fmt.Printf("Kind: %s, Name: %s/%s\n", item.GetKind(), item.GetName(), item.GetNamespace())
+
+		// Print spec.project
+		specProject, _, err := unstructured.NestedString(item.Object, "spec", "project")
+		if err != nil {
+			fmt.Printf("Error getting spec.project: %v\n", err)
+			return
+		}
+		specDestinationNamespace, _, err := unstructured.NestedString(item.Object, "spec", "destination", "namespace")
+		if err != nil {
+			fmt.Printf("Error getting spec.destination.namespace: %v\n", err)
+			return
+		}
+		specDestinationServer, _, err := unstructured.NestedString(item.Object, "spec", "destination", "server")
+		if err != nil {
+			fmt.Printf("Error getting spec.destination.server: %v\n", err)
+			return
+		}
+		specDestinationName, _, err := unstructured.NestedString(item.Object, "spec", "destination", "name")
+		if err != nil {
+			fmt.Printf("Error getting spec.destination.name: %v\n", err)
+			return
+		}
+
+		fmt.Printf("spec.project: %s\n", specProject)
+		fmt.Printf("spec.destination.namespace: %s\n", specDestinationNamespace)
+		fmt.Printf("spec.destination.name: %s\n", specDestinationName)
+		fmt.Printf("spec.destination.server: %s\n", specDestinationServer)
 	}
 }
