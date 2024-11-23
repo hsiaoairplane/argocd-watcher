8665cdcfb80682c3d3cfde0012e30ad7f5edb2ec
JenTing Hsiao <hsiaoairplane@gmail.com>
2024-11-23 21:57:36 +0800

    refactor

--- main.go
+++ main.go
@@ -15,19 +15,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
-func GetResourcesDynamically(dynamic dynamic.Interface, ctx context.Context, group string, version string, resource string, namespace string) ([]unstructured.Unstructured, error) {
-	resourceId := schema.GroupVersionResource{
-		Group:    group,
-		Version:  version,
-		Resource: resource,
-	}
-	list, err := dynamic.Resource(resourceId).Namespace(namespace).List(ctx, metav1.ListOptions{})
-	if err != nil {
-		return nil, err
-	}
-	return list.Items, nil
-}
-
 func main() {
 	ctx := context.Background()
 
